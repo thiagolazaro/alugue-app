644c9e4597d56c679e9bf76233267a6169daad11
Thiago Lázaro <thiagolazaro2009@gmail.com>
2021-10-05 22:53:51 -0300

    Finalizando o sc<New

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,93 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { ScrollView, TextInput } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+import New from '../components/New';
 
 export default function Home() {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      style={{backgroundColor: '#FFF', }}
+    >
+      {/* Header */}
+      <View style={styles.header}>
+        <View style={styles.InputArea}>
+          <Feather name="search" size={24} color="black" />
+          <TextInput 
+            placeholder="O que está procudando!"
+            style={styles.input}
+          />
+        </View>
+      </View>
+
+      {/* Menu */}
+      <View style={styles.contentMenu}>
+        <Text style={styles.title}>Novidades</Text>
+      </View>
+
+      {/* Area Novidades  */}
+      <ScrollView horizontal showsVerticalScrollIndicator={false} style={{ paddingHorizontal: 15, }}>
+        <New
+          cover={require('../assets/house1.jpg')}
+          name="Casa de Praia"
+          description="Casa nova uma quadra do mar,lugar seguro e monitorado 24horas. "
+          onPress={() => navigation.navigate('Detail')}
+        />
+        <New
+          cover={require('../assets/house2.jpg')}
+          name="Casa de BH"
+          description="Casa nova uma quadra do mar,lugar seguro e monitorado 24horas. "
+          onPress={() => {}}
+        />
+        <New
+          cover={require('../assets/house3.jpg')}
+          name="Casa de Pompéu"
+          description="Casa nova uma quadra do mar,lugar seguro e monitorado 24horas. "
+          onPress={() => {}}
+        />
+      </ScrollView>
+
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    paddingHorizontal: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    marginVertical: 20,    
+  },
+  InputArea: {
+    paddingHorizontal: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '98%',
+    backgroundColor: '#FFF',
+    elevation: 2,
+    height: 37,
+    borderRadius: 10,
+  },
+  input: {
+    fontFamily: 'Montserrat_500Medium',
+    paddingHorizontal: 10,
+    width: '90%'
+  },
+  contentMenu: {
+    flexDirection: 'row',
+    width: '100%',
+    alignItems: 'center'
+  },
+  title: {
+    paddingHorizontal: 15,
+    fontFamily: 'Montserrat_700Bold',
+    color: '#4f4a4a'
+  }
+});
--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Feather } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native';
 
 import Home from "./pages/home";
 import Detail from "./pages/detail";
@@ -12,8 +13,46 @@ function Routes() {
 	return(
 		<NavigationContainer>
 			<Stack.Navigator>
-				<Stack.Screen name="Home" component={Home} />
-				<Stack.Screen name="Detail" component={Detail} />
+				<Stack.Screen
+					name="Home"
+					component={Home}
+					options={{
+						title: 'ALUGUE',
+						headerTitleStyle: {
+							fontFamily: 'Montserrat_700Bold'
+						},
+
+						headerRight: () => (
+							<TouchableOpacity style={{ marginRight: 15 }}>
+								<Feather 
+								name="shopping-bag"
+								size={24}
+								color="black"
+								/>
+							</TouchableOpacity>
+						)
+					}}
+				/>
+				<Stack.Screen
+					name="Detail"
+					component={Detail}
+					options={{
+						title: 'DETALHE',
+						headerTitleStyle: {
+							fontFamily: 'Montserrat_700Bold'
+						},
+
+						headerRight: () => (
+							<TouchableOpacity style={{ marginRight: 15 }}>
+								<Feather 
+								name="shopping-bag"
+								size={24}
+								color="black"
+								/>
+							</TouchableOpacity>
+						)
+					}}
+				/>
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
